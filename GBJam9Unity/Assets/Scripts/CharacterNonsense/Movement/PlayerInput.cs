93b31c5b11a5a59e4c9cd04e271c9b83c4d4427e
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-09-23 01:26:32 +0100

    A button is interaction or attack.

--- GBJam9Unity/Assets/Scripts/CharacterNonsense/Movement/PlayerInput.cs
+++ GBJam9Unity/Assets/Scripts/CharacterNonsense/Movement/PlayerInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(EntityMover), typeof(PlayerAttack))]
+[RequireComponent(typeof(EntityMover), typeof(PlayerActions), typeof(PlayerComponents))]
 public class PlayerInput : MonoBehaviour
 {
     PlayerComponents Pcs;
@@ -35,12 +35,11 @@ public class PlayerInput : MonoBehaviour
     {
         if (Input.GetButtonDown("AButton"))
         {
-            Pcs.Attacker.DoAAttack();
             Pcs.Actions.AButtonPushed();
         }
         else if (Input.GetButtonDown("BButton"))
         {
-            Pcs.Attacker.DoBAttack();
+            Pcs.Actions.BButtonPushed();
         }
     }
 }
--- GBJam9Unity/Assets/Scripts/InteractableBase.cs
+++ GBJam9Unity/Assets/Scripts/InteractableBase.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableBase : MonoBehaviour
+public abstract class InteractableBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void RunInteraction();
 }
